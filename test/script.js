4c4d8e2e27ac5893e812cb282f8788e858b79427
Ashok Argent-Katwala <dev@ashok.org.uk>
2015-05-26 05:28:01 -0400

    Rename variables to prepare for adding another test string.

--- test/script.js
+++ test/script.js
@@ -2,37 +2,37 @@ var vows = require('vows'),
     assert = require('assert'),
     bleach = require('../lib/bleach');
 
-var HTML1 = 'This is <a href="#html">HTML</a> with a <script>\nvar x = 1;</script>SCRIPT',
-    HTML2 = 'This is <a href="#html">HTML</a> with a SCRIPT',
-    HTML3 = 'This is HTML with a SCRIPT';
+var HTML_LINK_SCRIPT = 'This is <a href="#html">HTML</a> with a <script>\nvar x = 1;</script>SCRIPT',
+    HTML_LINK = 'This is <a href="#html">HTML</a> with a SCRIPT',
+    HTML_PLAIN = 'This is HTML with a SCRIPT';
 
 vows.describe('script tests').addBatch({
 
   'whitelist mode': {
-    topic: function (){ return HTML1; },
+    topic: function (){ return HTML_LINK_SCRIPT; },
 
-    'eliminates script tags but keeps listed tags': function (HTML1){
-      var HTML = bleach.sanitize(HTML1, {mode: 'white', list:['a']});
-      assert.equal(HTML, HTML2);
+    'eliminates script tags but keeps listed tags': function (topic){
+      var HTML = bleach.sanitize(topic, {mode: 'white', list:['a']});
+      assert.equal(HTML, HTML_LINK);
     },
 
-    'eliminates all tags when given an empty list': function (HTML1){
-      var HTML = bleach.sanitize(HTML1, {mode: 'white', list:[]});
-      assert.equal(HTML, HTML3);
+    'eliminates all tags when given an empty list': function (topic){
+      var HTML = bleach.sanitize(topic, {mode: 'white', list:[]});
+      assert.equal(HTML, HTML_PLAIN);
     }
   },
 
   'blacklist mode': {
-    topic: function (){ return HTML1; },
+    topic: function (){ return HTML_LINK_SCRIPT; },
 
-    'eliminates script tags but keeps unlisted tags': function (HTML1){
-      var HTML = bleach.sanitize(HTML1, {mode: 'black', list:['script']});
-      assert.equal(HTML, HTML2);
+    'eliminates script tags but keeps unlisted tags': function (topic){
+      var HTML = bleach.sanitize(topic, {mode: 'black', list:['script']});
+      assert.equal(HTML, HTML_LINK);
     },
 
-    'eliminates all tags when all are blacklisted': function (HTML1){
-      var HTML = bleach.sanitize(HTML1, {mode: 'black', list:['a', 'script']});
-      assert.equal(HTML, HTML3);
+    'eliminates all tags when all are blacklisted': function (topic){
+      var HTML = bleach.sanitize(topic, {mode: 'black', list:['a', 'script']});
+      assert.equal(HTML, HTML_PLAIN);
     }
   }
 
